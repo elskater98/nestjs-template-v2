7430445cdb4cbed577fb9b1ba34da3c3cf840577
elskater98 <elskater98@gmail.com>
2022-03-18 23:09:12 +0100

    Add MongoDB Connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, {logger: ['error', 'warn', 'debug', 'log', 'verbose']});
+
+    app.useGlobalPipes(new ValidationPipe())
+
+    const config = app.get(ConfigService)
+
+    await app.listen(config.get<string>('SERVER_PORT', '3000'));
 }
+
 bootstrap();
